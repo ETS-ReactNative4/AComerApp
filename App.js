42d62ddb0a4ea293a7d24cc1b77666cedf890c2a
Alejandro Hernández <alejandrehl@icloud.com>
2019-07-06 13:38:03 -0400

    Home screen en vista principal

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import Home from "./app/screens/Home";
+
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Home />
     </View>
   );
 };
