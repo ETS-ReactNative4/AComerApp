69690fe1575095545c5858d711142facf2a9dd65
Alejandro Hernández <alejandrehl@icloud.com>
2019-07-06 13:49:19 -0400

    Primeros pasos con navegacion

---
+++ app/navigations/User.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+  createStackNavigator,
+  createAppContainer,
+  createBottomTabNavigator
+} from "react-navigation";
+import HomeScreen from "../screens/Home";
+import MyAccountScreen from "../screens/MyAccount";
+import SearchScreen from "../screens/Search";
+import TopFive from "../screens/TopFive";
+
+const User = () => {
+  return <div />;
+};
+
+export default User;
