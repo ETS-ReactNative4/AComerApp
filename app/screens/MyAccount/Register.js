7ba93a12a16448927bdc3f1e4b87d558981717f8
Alejandro Hernández <alejandrehl@icloud.com>
2019-07-08 03:31:36 -0400

    Avances con validacion de formulario

--- app/screens/MyAccount/Register.js
+++ app/screens/MyAccount/Register.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { Button } from "react-native-elements";
 
@@ -8,9 +8,30 @@ import { RegisterStruct, RegisterOptions } from "../../forms/Register";
 
 const Register = () => {
   const registerForm = useRef(null);
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    passwordConfirmation: ""
+  });
 
-  onSubmit = () => {
-    const validate = registerForm.current.getValue();
+  const { name, email, password, passwordConfirmation } = user;
+
+  const onChange = formData => setUser(formData);
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (password !== passwordConfirmation) {
+      console.log("Tus contraseñas no coinciden.");
+    } else {
+      //register({ name, email, password })
+      const validate = registerForm.current.getValue();
+      if (validate) {
+        console.log("Form succes");
+      } else {
+        console.log("Form Error");
+      }
+    }
   };
 
   return (
@@ -19,6 +40,8 @@ const Register = () => {
         ref={registerForm}
         type={RegisterStruct}
         options={RegisterOptions}
+        value={user}
+        onChange={onChange}
       />
       <Button title="¡Unirse!" onPress={onSubmit} />
     </View>
