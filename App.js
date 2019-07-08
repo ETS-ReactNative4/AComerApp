91d3372ad108ee736a836f87b67a977ae0802cee
Alejandro Hernández <alejandrehl@icloud.com>
2019-07-08 05:25:35 -0400

    inicializando firebase en app.js

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-
 import UserNavigation from "./app/navigations/User";
 import AuthState from "./app/context/auth/AuthState";
+
+import firebaseConfig from "./app/utils/Firebase";
+import * as firebase from "firebase";
+firebase.initializeApp(firebaseConfig);
 
 const App = () => {
   return (
