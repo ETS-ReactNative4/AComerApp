daa7965cfc85844bc3c614399f66576433285cab
Alejandro Hernández <alejandrehl@icloud.com>
2019-07-08 07:15:34 -0400

    Implementando logica

--- app/screens/MyAccount/Login.js
+++ app/screens/MyAccount/Login.js
@@ -1,13 +1,41 @@
 import React, { useRef, useState, useContext } from "react";
 import { StyleSheet, View, Text, ActivityIndicator } from "react-native";
 import { Image, Button } from "react-native-elements";
+import Toast, { DURATION } from "react-native-easy-toast";
+import AuthContext from "../../context/auth/authContext";
 
 import t from "tcomb-form-native";
 const Form = t.form.Form;
 import { LoginStruct, LoginOptions } from "../../forms/Login";
 
+import * as firebase from "firebase";
+
 const Login = () => {
   const loginForm = useRef(null);
+  const toast = useRef(null);
+  const authContext = useContext(AuthContext);
+  const { setError, error } = authContext;
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  });
+
+  const { email, password } = user;
+
+  const onChange = formData => setUser(formData);
+
+  const onSubmit = () => {
+    if (email === "" || password === "") {
+      setError("Debes completar todos los campos");
+    } else {
+      const validate = loginForm.current.getValue();
+      if (validate) {
+      } else {
+        setError("Formulario Inválido");
+      }
+    }
+  };
+
   return (
     <View style={styles.viewBody}>
       <Image
@@ -18,8 +46,28 @@ const Login = () => {
         resizeMode="contain"
       />
       <View style={styles.viewForm}>
-        <Form ref={loginForm} type={LoginStruct} options={LoginOptions} />
-        <Button title="¡Acceder!" buttonStyle={styles.buttonLoginContainer} />
+        <Form
+          ref={loginForm}
+          type={LoginStruct}
+          options={LoginOptions}
+          value={user}
+          onChange={onChange}
+        />
+        <Button
+          title="¡Acceder!"
+          buttonStyle={styles.buttonLoginContainer}
+          onPress={onSubmit}
+        />
+        <Text style={styles.formErrorMessage}>{error}</Text>
+        <Toast
+          ref={toast}
+          position="bottom"
+          positionValue={250}
+          fadeInDuration={750}
+          fadeOutDuration={1000}
+          opacity={0.8}
+          textStyle={{ color: "#fff" }}
+        />
       </View>
     </View>
   );
@@ -46,6 +94,11 @@ const styles = StyleSheet.create({
     marginTop: 20,
     marginLeft: 10,
     marginRight: 10
+  },
+  formErrorMessage: {
+    color: "#f00",
+    textAlign: "center",
+    marginTop: 30
   }
 });
 
