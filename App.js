992f9dde76d17835f8e9bfa0f003b720d6518673
Alejandro Hernández <alejandrehl@icloud.com>
2019-07-09 03:01:24 -0400

    Guardando Toke en async storage al iniciar la app

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, View } from "react-native";
 import UserNavigation from "./app/navigations/User";
 import AuthState from "./app/context/auth/AuthState";
+import setAuthToken from "./app/utils/setAuthToken";
+import { AsyncStorage } from "react-native";
 
 import firebaseConfig from "./app/utils/Firebase";
 import * as firebase from "firebase";
 firebase.initializeApp(firebaseConfig);
+
+AsyncStorage.getItem("token", (err, result) => {
+  if (!err) {
+    setAuthToken(result);
+  }
+});
 
 const App = () => {
   return (
