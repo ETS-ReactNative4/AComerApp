64b89987374461cef2c620f8c1d358647332664f
Alejandro Hernández <alejandrehl@icloud.com>
2019-07-08 23:27:31 -0400

    MEjorando el auth context

--- app/context/auth/AuthState.js
+++ app/context/auth/AuthState.js
@@ -1,18 +1,82 @@
 import React, { useReducer } from "react";
 import AuthContext from "./authContext";
 import authReducer from "./authReducer";
-import { SET_ERROR, REMOVE_ERROR } from "../types";
+import {
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  SET_ERROR,
+  REMOVE_ERROR
+} from "../types";
+import { AsyncStorage } from "react-native";
+import api from "../../services/ApiConnection";
 
 const AuthState = props => {
   const initialState = {
-    user: null,
+    token: AsyncStorage.getItem("token"),
     isAuthenticated: null,
-    error: null
+    loading: true,
+    error: null,
+    user: null
   };
 
   const [state, dispatch] = useReducer(authReducer, initialState);
 
+  // LOAD USER
+  const loadUser = async () => {
+    if (AsyncStorage.token) {
+      setAuthToken(AsyncStorage.token);
+    }
+
+    try {
+      const res = await api.get("/api/auth");
+      dispatch({
+        type: USER_LOADED,
+        payload: res.data
+      });
+    } catch (err) {
+      dispatch({
+        type: AUTH_ERROR
+      });
+    }
+  };
+
   // LOGIN USER
+  const login = async formData => {
+    try {
+      const res = await api.post("/api/auth", formData);
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      });
+
+      loadUser();
+    } catch (err) {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: err.response.data.msg
+      });
+    }
+  };
+
+  // Register User
+  const register = async formData => {
+    try {
+      const res = await api.post("/api/users", formData);
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data
+      });
+
+      loadUser();
+    } catch (err) {
+      dispatch({
+        type: REGISTER_FAIL,
+        payload: err.response.data.msg
+      });
+    }
+  };
 
   // SET ERROR
   const setError = msg => {
@@ -23,8 +87,15 @@ const AuthState = props => {
   return (
     <AuthContext.Provider
       value={{
+        token: state.token,
+        isAuthenticated: state.isAuthenticated,
+        loading: state.loading,
+        user: state.user,
         error: state.error,
-        setError
+        setError,
+        loadUser,
+        login,
+        register
       }}
     >
       {props.children}
--- app/context/auth/authReducer.js
+++ app/context/auth/authReducer.js
@@ -1,7 +1,28 @@
-import { SET_ERROR, REMOVE_ERROR } from "../types";
+import {
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  SET_ERROR,
+  REMOVE_ERROR
+} from "../types";
 
 export default (state, action) => {
   switch (action.type) {
+    case USER_LOADED:
+      return console.log("USER LOADED REDUCER");
+    case AUTH_ERROR:
+      return console.log("AUTH ERROR REDUCER");
+    case LOGIN_SUCCESS:
+      return console.log("LOGIN SUCCESS REDUCER");
+    case LOGIN_FAIL:
+      return console.log("LOGIN FAIL REDUCER");
+    case REGISTER_SUCCESS:
+      return console.log("REGISTER SUCCESS REDUCER");
+    case REGISTER_FAIL:
+      return console.log("REGISTER FAIL REDUCER");
     case SET_ERROR:
       return {
         ...state,
