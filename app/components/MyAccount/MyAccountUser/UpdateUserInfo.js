294571b74cd003e7a88a94c11036289102e11853
Alejandro Hernández <alejandrehl@icloud.com>
2019-07-10 04:46:40 -0400

    Avanzando en update user

--- app/components/MyAccount/MyAccountUser/UpdateUserInfo.js
+++ app/components/MyAccount/MyAccountUser/UpdateUserInfo.js
@@ -1,14 +1,49 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
+import { ListItem } from "react-native-elements";
 
 const UpdateUserInfo = () => {
+  const menuItems = [
+    {
+      title: "Cambiar Nombre y Apellido",
+      iconType: "material-community",
+      iconNameRight: "chevron-right",
+      iconColorRight: "#ccc",
+      iconNameLeft: "account-circle",
+      iconColorLeft: "#ccc",
+      onPress: () => console.log("Click en cambiar nombre y apellido")
+    }
+  ];
+
   return (
     <View>
-      <Text>Update user info</Text>
+      {menuItems.map((item, index) => (
+        <ListItem
+          key={index}
+          title={item.title}
+          leftIcon={{
+            type: item.iconType,
+            name: item.iconNameLeft,
+            color: item.iconColorLeft
+          }}
+          rightIcon={{
+            type: item.iconType,
+            name: item.iconNameRight,
+            color: item.iconColorRight
+          }}
+          onPress={item.onPress}
+          containerStyle={styles.contentContainerStyle}
+        />
+      ))}
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  contentContainerStyle: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#e3e3e3"
+  }
+});
 
 export default UpdateUserInfo;
