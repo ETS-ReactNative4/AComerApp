1255c87bb02ae71b22750437946bd5ce4e5423e5
Alejandro Hernández <alejandrehl@icloud.com>
2019-07-10 05:04:42 -0400

    Save

---
+++ app/components/Elements/OverlayOneInput.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { Overlay, Input, Button } from "react-native-elements";
+
+const OverlayOneInput = () => {
+  return (
+    <View>
+      <Text>OverlayOneInput</Text>
+    </View>
+  );
+};
+
+export default OverlayOneInput;
