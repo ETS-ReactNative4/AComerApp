0f8976008836129aa43a0da97fc8cce9c187f454
Alejandro Hernández <alejandrehl@icloud.com>
2019-07-14 15:56:28 -0400

    Ocultando correctamente el boton par aagregar un restaurante cuando el usuario no esta logeado

--- app/screens/Restaurants/Restaurants.js
+++ app/screens/Restaurants/Restaurants.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import AuthContext from "../../context/auth/authContext";
 import { StyleSheet, View, Text } from "react-native";
 import ActionButton from "react-native-action-button";
 
 const Restaurants = ({ navigation }) => {
+  const authContext = useContext(AuthContext);
+  const { loadUser, isAuthenticated } = authContext;
+
+  useEffect(() => {
+    loadUser();
+  }, [isAuthenticated]);
+
   const goToScreen = name => {
     navigation.navigate(name);
   };
@@ -10,10 +18,12 @@ const Restaurants = ({ navigation }) => {
   return (
     <View style={styles.viewBody}>
       <Text>Restaurants Screen</Text>
-      <ActionButton
-        buttonColor="#ffc107"
-        onPress={() => goToScreen("AddRestaurant")}
-      />
+      {isAuthenticated && (
+        <ActionButton
+          buttonColor="#ffc107"
+          onPress={() => goToScreen("AddRestaurant")}
+        />
+      )}
     </View>
   );
 };
