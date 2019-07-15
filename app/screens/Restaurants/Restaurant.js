00e08b191d026013414b70fdd105a44bd9875cd2
Alejandro Hernández <alejandrehl@icloud.com>
2019-07-14 23:39:29 -0400

    BAse

--- app/screens/Restaurants/Restaurant.js
+++ app/screens/Restaurants/Restaurant.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { StyleSheet, View, Text, ActivityIndicator } from "react-native";
 import { Image, Icon, ListItem } from "react-native-elements";
-import { ResizeMode } from "expo-av/build/Video";
 
 const Restaurant = ({ navigation }) => {
   const { name, description, address, city, image } = navigation.state.params;
@@ -14,6 +13,13 @@ const Restaurant = ({ navigation }) => {
           PlaceholderContent={<ActivityIndicator />}
           style={styles.imageRestaurant}
         />
+      </View>
+      <View style={styles.viewRestaurantBasicInfo}>
+        <Text style={styles.nameRestaurant}>{name}</Text>
+        <Text style={styles.descriptionRestaurant}>{description}</Text>
+        <Text style={styles.addressRestaurant}>
+          {address}, {city}
+        </Text>
       </View>
     </View>
   );
@@ -30,6 +36,21 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 200,
     resizeMode: "cover"
+  },
+  viewRestaurantBasicInfo: {
+    margin: 15
+  },
+  nameRestaurant: {
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+  descriptionRestaurant: {
+    marginTop: 5,
+    color: "grey"
+  },
+  addressRestaurant: {
+    marginTop: 5,
+    color: "grey"
   }
 });
 
