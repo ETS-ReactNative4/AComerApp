6badbda9aa497eec5afebe8b4966bb1c55ef5738
Alejandro Hernández <alejandrehl@icloud.com>
2019-07-15 18:50:26 -0400

    Save

--- app/screens/TopFive.js
+++ app/screens/TopFive.js
@@ -1,19 +1,10 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
-import api from "../utils/ApiConnection";
-import { Button } from "react-native-elements";
 
 const TopFive = () => {
-  const getFeaturedBranches = async () => {
-    try {
-      const res = await api.get("/api/featuredbranches/");
-    } catch (err) {}
-  };
-
   return (
     <View style={styles.viewBody}>
       <Text>TopFive Screen</Text>
-      <Button title="Get Branches" onPress={getFeaturedBranches} />
     </View>
   );
 };
