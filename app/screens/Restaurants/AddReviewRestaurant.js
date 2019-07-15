1d773b063da0af74c60e1a2424c5527dc0454257
Alejandro Hernández <alejandrehl@icloud.com>
2019-07-15 00:15:41 -0400

    COmponente add review

---
+++ app/screens/Restaurants/AddReviewRestaurant.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const AddReviewRestaurant = () => {
+  return (
+    <View style={styles.viewBody}>
+      <TexT>Add Review to Restaurant</TexT>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  viewBody: {
+    flex: 1
+  }
+});
+
+export default AddReviewRestaurant;
